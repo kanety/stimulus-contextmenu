fd7f625c7dbbb73232374ea3fd1d545bb2295a05
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2024-06-26 11:31:27 +0900

    Update devDependencies

--- dist/index.modern.mjs
+++ dist/index.modern.mjs
@@ -1,2 +1,2 @@
 import{Controller as t}from"@hotwired/stimulus";import"@kanety/stimulus-static-actions";class e extends t{show(t){this.hasContextTarget&&!this.contextTarget.contains(t.target)||(this.menuTarget.style.top=`${t.pageY+1}px`,this.menuTarget.style.left=`${t.pageX+1}px`,this.toggleClass(!0),t.preventDefault())}hide(t){this.toggleClass(!1)}toggleClass(t){t?this.menuTarget.classList.add("st-contextmenu--visible"):this.menuTarget.classList.remove("st-contextmenu--visible")}}e.targets=["menu","context"],e.actions=[["element","contextmenu->show"],["element","click@window->hide"]];export{e as default};
-//# sourceMappingURL=index.modern.js.map
+//# sourceMappingURL=index.modern.mjs.map
