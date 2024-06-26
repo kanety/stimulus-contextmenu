ca5baca63a73b8eae34dadb97d948b8c3d6af38d
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2024-06-26 11:33:39 +0900

    Fix test and examples

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,3 @@
-global.$ = document.querySelector.bind(document);
-global.$$ = document.querySelectorAll.bind(document);
-
 import { Application } from '@hotwired/stimulus';
 import MenuController from '@kanety/stimulus-menu';
 import ContextmenuController from 'index';
@@ -9,3 +6,5 @@ const application = Application.start();
 application.register('menu', MenuController);
 application.register('contextmenu', ContextmenuController);
 
+global.$ = document.querySelector.bind(document);
+global.$$ = document.querySelectorAll.bind(document);
